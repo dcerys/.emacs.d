030040fe1df2911a0d44fac5c4d4d07560d20fe1
Dan Cerys <dcerys@dollabs.com>
2016-08-18 13:03:48 -0400

    put pamela files in clojure mode

--- elisp/setup-clojure.el
+++ elisp/setup-clojure.el
@@ -81,3 +81,6 @@
      (define-key clojure-mode-map (kbd "C-M-r") 'cider-refresh)
      (define-key clojure-mode-map (kbd "C-c u") 'cider-user-ns)
      (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns)))
+
+;; pamela specific
+(add-to-list 'auto-mode-alist '("\\.pamela$" . clojure-mode))
